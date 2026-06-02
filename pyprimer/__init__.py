69a4faad00cf58be12cdd056fb00d7044aa64a0e
Ryan Ridenbaugh <ridenbaughryan@gmail.com>
2026-06-01 20:26:08 -0400

    submitted to PyPI

--- pyprimer/__init__.py
+++ pyprimer/__init__.py
@@ -1,3 +1,9 @@
+"""
+PyPrimer: A bioinformatic pipeline for identifying intron length variation
+across species to design diagnostic multiplex PCR primers. Applicable to any system
+with an NCBI annotated reference genome.
+"""
+
 __version__="0.1.0"
 __author__="Ryan Ridenbaugh"
 __email__="ridenbaughryan@gmail.com"
